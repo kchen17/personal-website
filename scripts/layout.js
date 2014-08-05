51159e221f26fa497a755ab6eb26fabcacb9bc16
Kathy <kchen2013@gmail.com>
2014-08-05 17:14:21 -0400

    basic website framework

---
+++ scripts/layout.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	slidescroll();
+	openMenu();
+});
+
+
+function slidescroll() {
+	$('a[href*=#]:not([href=#])').click(function() {
+		var hostname = location.hostname == this.hostname;
+		var pathname = location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'');
+		if (pathname && hostname) {
+			var target = $(this.hash);
+			targe = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 200);
+				return false;
+			}
+		}
+	});
+};
+
+function openMenu() {
+	$('#panel > h2').click(function() {
+		$('ul').toggleClass('out');
+	});
+	$('a.menu').click(function() {
+		$('ul').toggleClass('out');
+	});
+
+};
